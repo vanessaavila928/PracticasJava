76b839017c48ed5f3aff1f6d38686fa2dddc7a7b
vanessa <vanessaavila928@gmail.com>
2025-04-04 11:57:26 -0600

    nuevo archivo

--- Curso1/password.java
+++ Curso1/password.java
@@ -2,15 +2,12 @@ public class password{
     public static void main(String[] args) {
         String password = "SeCrETo92@";
 
-        if(password.equals("SeCrETo92@")){
+        if(password.equals("SeCreTo92@")){
             System.out.println("La contraseña es correcta");
         }else{
             System.out.println("La contraseña es incorrecta");
         }
 
-
-
-
-    }
+    } // este puede ser utilizado para contraseñas ya que si importan mayusculas y minusculas
 
 }

f5368b2c5360a15d31ecb164dd7746db2ac1bab1
vanessa <vanessaavila928@gmail.com>
2025-04-04 11:55:42 -0600

    nuevo archivo

---
+++ Curso1/password.java
@@ -0,0 +1,16 @@
+public class password{
+    public static void main(String[] args) {
+        String password = "SeCrETo92@";
+
+        if(password.equals("SeCrETo92@")){
+            System.out.println("La contraseña es correcta");
+        }else{
+            System.out.println("La contraseña es incorrecta");
+        }
+
+
+
+
+    }
+
+}
